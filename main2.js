4b09d835f56fe2aa349f119dd574af521ebe6ab7
eloaandreatta <111518085+eloaandreatta@users.noreply.github.com>
2024-03-26 12:19:03 -0300

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,18 @@
+function atualizarTempo(){
+    var display = document.querySelector('.contador');
+    var agora = new Date();
+    var horario = corrigirHorario(agora.getHours()) + ':' + corrigirHorario(agora.getMinutes()) + ':' + corrigirHorario(agora.getSeconds());
+    display.textContent = horario;
+}
+
+function corrigirHorario(numero){
+    if (numero < 10){
+        numero = '0' + numero;
+    }
+    return numero;
+}
+
+atualizarTempo();
+setInterval(atualizarTempo, 1000)
+
+console.log(horario);
